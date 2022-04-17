6edb796fa6ddcf41a2432b3b517222ea266f00d5
Alinka7 <alinkamyronets66@gmail.com>
2022-04-17 12:13:59 +0300

    adding a task

--- lucky.py
+++ lucky.py
@@ -8,3 +8,22 @@ for Sierentz Global Merchants
 
 Test task
 """
+
+import re
+
+def lucky(sequence):
+    result = re.findall(r'[56|65]{2,}', sequence)
+    if not result:
+        return 0
+    l = []
+    for i in result:
+        if re.search('^[5]*$', i) or re.search('^[6]*$', i):
+            return 0
+        else:
+            l.append(i)
+    return max(l)
+            
+
+lucky('4556432455665334')
+
+
